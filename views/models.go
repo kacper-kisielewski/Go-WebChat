498ab6619911467bf128e72ea8c41fd65153c9a7
Kacperek <kacperek@localhost.localdomain>
2020-10-31 19:38:23 +0100

    bug fixes

--- views/models.go
+++ views/models.go
@@ -3,12 +3,12 @@ package views
 //RegisterBody model
 type RegisterBody struct {
 	Username string `form:"username" binding:"required,username"`
-	Email    string `form:"email" biding:"required,email"`
+	Email    string `form:"email" binding:"required,email"`
 	Password string `form:"password"`
 }
 
 //LoginBody struct
 type LoginBody struct {
-	Email    string `form:"email" biding:"required,email"`
+	Email    string `form:"email" binding:"required,email"`
 	Password string `form:"password"`
 }
